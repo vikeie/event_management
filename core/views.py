9de0e3adc44222ac8dc41f092ac81605767d328d
Vignesh Soundarajan --CNTR <Vignesh_Soundarajan@contractor.amat.com>
2021-01-05 12:31:26 +0530

    added event models and serializers in views

--- core/views.py
+++ core/views.py
@@ -6,7 +6,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response 
 from rest_framework.permissions import IsAuthenticated 
 
-from .serializers import RegisterSerializer, UserSerializer
+from .serializers import RegisterSerializer, UserSerializer, EventSerializer
+from .models import Event
 
 # Create your views here.
 
